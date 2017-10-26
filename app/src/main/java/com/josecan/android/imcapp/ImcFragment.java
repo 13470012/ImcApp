2cb740e793a8fbeec73aba3fd78eec698d444c1d
jose.can <jose.can@itrends.com.mx>
2017-10-26 12:00:19 -0500

    Version inicial del proyecto. No incluye eventos

---
+++ app/src/main/java/com/josecan/android/imcapp/ImcFragment.java
@@ -0,0 +1,30 @@
+package com.josecan.android.imcapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jose.can on 26/10/2017.
+ */
+
+public class ImcFragment extends Fragment {
+    public ImcFragment() {
+        super();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragmento_imc, container, false);
+        return  v;
+    }
+}
